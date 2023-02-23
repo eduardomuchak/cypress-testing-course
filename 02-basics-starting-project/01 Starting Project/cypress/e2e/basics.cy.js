68e851c15b35231ae8372377d22aeff40c5cb522
Eduardo Muchak <eduardomuchak@gmail.com>
2023-02-23 10:56:42 -0300

    test: should display the page title

--- 02-basics-starting-project/01 Starting Project/cypress/e2e/basics.cy.js
+++ 02-basics-starting-project/01 Starting Project/cypress/e2e/basics.cy.js
@@ -3,6 +3,12 @@
 describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://localhost:5173');
-    cy.get('h1').should('have.text', 'React Tasks');
+    cy.get('.main-header img').should('be.visible');
+  });
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('.main-header h1').should('be.visible');
+    cy.get('.main-header h1').contains('React Tasks');
   });
 });
