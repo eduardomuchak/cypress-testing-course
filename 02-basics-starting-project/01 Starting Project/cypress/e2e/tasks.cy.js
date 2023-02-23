06bb03f26e9ba7df261b77b7bb730179dfe3f7eb
Eduardo Muchak <eduardomuchak@gmail.com>
2023-02-23 14:51:14 -0300

    test: should create a new task

--- 02-basics-starting-project/01 Starting Project/cypress/e2e/tasks.cy.js
+++ 02-basics-starting-project/01 Starting Project/cypress/e2e/tasks.cy.js
@@ -11,4 +11,28 @@ describe('tasks management', () => {
     cy.get('.backdrop').click({ force: true });
     cy.get('.backdrop').should('not.exist');
   });
+
+  it('should create a new task', () => {
+    cy.visit('http://localhost:5173');
+    // Abrir modal
+    cy.get('button').contains('Add Task').click();
+    cy.get('dialog').should('be.visible');
+
+    // Inserir dados
+    cy.get('input[id="title"]').type('My new task');
+    cy.get('textarea[id="summary"]').type('Some description for my new task');
+
+    // Selecionar opção do select
+    cy.get('select[id="category"]').select('low');
+
+    // Clicar no botão de salvar
+    cy.get('button').contains('Add Task').click();
+
+    // Verifica se o modal foi fechado e se a nova tarefa foi criada
+    cy.get('dialog').should('not.exist');
+    cy.get('li div h2').contains('My new task').should('exist');
+    cy.get('li div p')
+      .contains('Some description for my new task')
+      .should('exist');
+  });
 });
