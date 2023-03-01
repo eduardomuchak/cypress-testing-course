b6f3308443174b41c18c4cf3d6b48b43516c5fef
Eduardo Muchak <eduardomuchak@gmail.com>
2023-03-01 16:01:15 -0300

    test: :test_tube: takeaways tests

---
+++ 06-network-auth-starting-project/01 Starting Project/public/build/routes/login-NUMTBTID.js
@@ -0,0 +1,30 @@
+import {
+  Auth_default,
+  require_validation
+} from "/build/_shared/chunk-6IUAAYPU.js";
+import {
+  require_auth
+} from "/build/_shared/chunk-V2UFASCO.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-43ZBSOTS.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-5KL4PAQL.js";
+
+// app/routes/login.jsx
+var import_auth = __toESM(require_auth());
+var import_validation = __toESM(require_validation());
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function LoginRoute() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Auth_default, { mode: "login" }, void 0, false, {
+    fileName: "app/routes/login.jsx",
+    lineNumber: 8,
+    columnNumber: 10
+  }, this);
+}
+var login_default = LoginRoute;
+export {
+  login_default as default
+};
+//# sourceMappingURL=/build/routes/login-NUMTBTID.js.map
---
+++ 06-network-auth-starting-project/01 Starting Project/public/build/routes/signup-75ZUBMGA.js
@@ -0,0 +1,30 @@
+import {
+  Auth_default,
+  require_validation
+} from "/build/_shared/chunk-6IUAAYPU.js";
+import {
+  require_auth
+} from "/build/_shared/chunk-V2UFASCO.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-43ZBSOTS.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-5KL4PAQL.js";
+
+// app/routes/signup.jsx
+var import_auth = __toESM(require_auth());
+var import_validation = __toESM(require_validation());
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function SignupRoute() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Auth_default, { mode: "signup" }, void 0, false, {
+    fileName: "app/routes/signup.jsx",
+    lineNumber: 8,
+    columnNumber: 10
+  }, this);
+}
+var signup_default = SignupRoute;
+export {
+  signup_default as default
+};
+//# sourceMappingURL=/build/routes/signup-75ZUBMGA.js.map
